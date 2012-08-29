9f614db4641fd5c798c630671afd9ad1474c513a
David Ketcheson <dketch@gmail.com>
2012-08-29 22:17:13 +0100

    Remove an unnecessary file and simplify check_RK_order.m.

--- RK-coeff-opt/check_RK_order.m
+++ RK-coeff-opt/check_RK_order.m
@@ -1,37 +1,28 @@
 function p = check_RK_order(A,b,c,problem_class)
 % function p = check_RK_order(A,b,c)
-% Determines order of a RK method, up to sixth order
-% For an s-stage method, input A should be a s x s matrix;
-% b and c should are column vectors of length s
+% Determines order of a RK method, up to sixth order.
+%
+% For an s-stage method, input `A` should be a `s \times s` matrix;
+% `b` and `c` should be column vectors of length `s`.
 
-done=0;
-p=0;
 eps = 1.e-14;
 
-if strcmp(problem_class,'nonlinear')
-     % order 1
-    if abs(sum(b)-1)<eps
-      p = 1;
-    else
-      done=1;
-    end
-    while done==0
-        cond=oc_butcher(A,b,c,p+1);
-        if max(abs(cond))<eps
-            p=p+1;
-        else
-            done=1;
-        end
-    end
+if abs(sum(b)-1)<eps
+    p = 1;
 else
-    while done==0
-        linear_cond = linear_order_conditions(A,b,p+1);
-        if abs(linear_cond)<eps
-            p=p+1;
-        else
-            done=1;
-        end
+    p = 0;
+    return
+end
+
+while p<10
+    if strcmp(problem_class,'nonlinear')
+        cond=oc_butcher(A,b,c,p+1);
+    else
+        cond = b'*A^(p)*ones(length(b),1) - 1/factorial(p+1);
+    end
+    if max(abs(cond))<eps
+        p=p+1;
+    else
+        return
     end
 end
-        
-    
