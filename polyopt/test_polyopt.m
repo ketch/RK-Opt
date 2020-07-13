8a73158b1000bf6d2764d9f99d8aaa2366580f90
Hendrik Ranocha <mail@ranocha.de>
2020-07-13 09:03:55 +0200

    remove randomness from test_polyopt

--- polyopt/test_polyopt.m
+++ polyopt/test_polyopt.m
@@ -6,21 +6,21 @@ tol = 1.e-2;
 
 %% Test real axis monomial
 lambda = spectrum('realaxis', 100);
-s = randi(6);
+s = 5;
 p = 1;
 [h, poly_coeff] = opt_poly_bisect(lambda, s, p, 'monomial');
 assert(all(ismembertol(h, 2*s^2, tol, 'ByRows', true)))
 
 %% Test real axis Chebyshev
 lambda = spectrum('realaxis', 100);
-s = randi(10);
+s = 8;
 p = 1;
 [h, poly_coeff] = opt_poly_bisect(lambda, s, p, 'chebyshev');
 assert(all(ismembertol(h, 2*s^2, tol, 'ByRows', true)))
 
 %% Test imaginary axis monomial
 lambda = spectrum('imagaxis', 100);
-s = randi(5);
+s = 4;
 p = 1;
 [h, poly_coeff] = opt_poly_bisect(lambda, s, p, 'monomial');
 assert(all(ismembertol(h, s-1, tol, 'ByRows', true)))
@@ -28,14 +28,14 @@ assert(all(ismembertol(h, s-1, tol, 'ByRows', true)))
 %% Test imaginary axis Chebyshev
 % For some reason, SDPT3 and Sedumi sometimes fail for small s
 lambda = spectrum('imagaxis', 100);
-s = randi(10)+10;
+s = 8;
 p = 1;
 [h, poly_coeff] = opt_poly_bisect(lambda, s, p, 'rotated chebyshev')
 assert(all(ismembertol(h, s-1, tol, 'ByRows', true)))
 
 %% Test disk monomial
 lambda = spectrum('disk', 100);
-s = randi(5);
+s = 5;
 p = 1;
 [h, poly_coeff] = opt_poly_bisect(lambda, s, p, 'monomial')
 assert(all(ismembertol(h, s, tol, 'ByRows', true)))
@@ -43,7 +43,7 @@ assert(all(ismembertol(h, s, tol, 'ByRows', true)))
 %% Test disk binomial
 % For some reason, SDPT3 and Sedumi sometimes fail for small s
 lambda = spectrum('disk', 100);
-s = randi(10)+10;
+s = 10;
 p = 2;
 [h, poly_coeff] = opt_poly_bisect(lambda, s, p, 'binomial')
 assert(all(ismembertol(h, s-1, tol, 'ByRows', true)))
