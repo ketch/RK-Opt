c3a57e1ec80c2de4892004f9502c530a81c182ba
David Ketcheson <dketch@gmail.com>
2026-02-26 17:24:06 +0300

    Custom unpacker for 2N methods.

---
+++ RK-coeff-opt/unpack_2N.m
@@ -0,0 +1,22 @@
+function [A,b,c,A_2N,B_2N]=unpack_2N(X)
+    % n = 2s - 1 free parameters
+    s = (length(X)+1)/2; % # of stages
+    % for 2N methods:
+    % X = [A_2, A_3, ..., A_s, B_1, ..., B_s]
+    A_2N = [0 X(1:s-1)];
+    B_2N = X(s:end);
+    K = zeros(s+1,s);
+    for i=1:s-1
+        K(i+1,i) = B_2N(i);
+        for j = 2:s-i+1
+            for k=0:j-1
+                K(i+j,i) = K(i+j,i) + B_2N(i+k)*prod(A_2N(i+1:i+k));
+            end
+        end
+    end
+    A = K(1:s,:);
+    b = K(s+1,:); b=b';
+    c=sum(A,2);
+    alpha=zeros(s+1,s); beta=zeros(s+1,s); % Not yet implemented
+
+
--- RK-coeff-opt/unpack_lsrk.m
+++ RK-coeff-opt/unpack_lsrk.m
@@ -50,26 +50,6 @@ switch class
     %     gamma_{53} ... gamma_{s+1,3} delta_s delta_{s+1} delta_{s+2} ]
     delta =[1 X(s:2*s-3) X(end-3:end-1)];
     betahat = X(end);
-  case '2N'
-    % n = 2s - 1 free parameters
-    s = (length(X)+1)/2; % # of stages
-    % for 2N methods:
-    % X = [A_2, A_3, ..., A_s, B_1, ..., B_s]
-    A_2N = [0 X(1:s-1)];
-    B_2N = X(s:end);
-    K = zeros(s+1,s);
-    for i=1:s-1
-        K(i,i+1) = B_2N(i);
-        for j = 2:s-i+1
-            for k=0:j-1
-                K(i+j,i) = K(i+j,i) + B_2N(i+k)*prod(A_2N(i+1:i+k));
-            end
-        end
-    end
-    A = K(1:s,:);
-    b = K(s+1,:); b=b';
-    c=sum(A,2);
-    alpha=zeros(s+1,s); beta=zeros(s+1,s); % Not yet implemented
 
 end
 
